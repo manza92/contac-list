0bb4ae866b7190e3d4d531b331bb58ddaa25bee8
manza92 <138921218+manza92@users.noreply.github.com>
2023-09-10 16:53:00 -0600

    Update listcontact.js

--- version2/listcontact.js
+++ version2/listcontact.js
@@ -1,31 +1,14 @@
-// Crear una lista de contactos con datos predefinidos
-let contactos = [
-  { nombre: "Juan Perez" },
-  { nombre: "Maria Garcia" },
-  { nombre: "Pedro Rodriguez" }
-];
-
-// Función para añadir un nuevo contacto a la lista
-function agregarContacto(nombre) {
-  contactos.push({ nombre });
+class Contacto {
+  constructor(id, nombres, apellidos, telefono, ciudad, direccion) {
+    this.id = id;
+    this.nombres = nombres;
+    this.apellidos = apellidos;
+    this.telefono = telefono;
+    this.ubicaciones = {
+      ciudad: ciudad,
+      direccion: direccion
+    };
+  }
 }
-
-// Función para borrar un contacto existente de la lista
-function borrarContacto(nombre) {
-  contactos = contactos.filter(contacto => contacto.nombre !== nombre);
-}
-
-// Función para imprimir en consola los contactos presentes en la lista
-function imprimirContactos() {
-  console.log("Lista de contactos:");
-  contactos.forEach(contacto => console.log(contacto.nombre));
-}
-
-// Agregar un nuevo contacto a la lista
-agregarContacto("Ana Lopez");
-
-// Borrar un contacto existente de la lista
-borrarContacto("Maria Garcia");
-
-// Imprimir en consola los contactos presentes en la lista
-imprimirContactos();
+let contacto1 = new Contacto(1, "Juan", "Pérez", "555-1234", "Tegucigalpa", "Colonia Palmira");
+let contacto2 = new Contacto(2, "María", "García", "555-5678", "San Pedro Sula", "Barrio El Benque");
